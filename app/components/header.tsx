9a0cef4c896cbcd972330786e5625a9e36d8320e
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-10 20:03:07 +0900

    add: Headerコンポーネントにナビゲーションメニューを追加

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,24 @@
 export const Header: React.FC = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <nav>
+        <div>
+          <ul className="flex justify-center items-center gap-4">
+            <li>
+              <a href="#profile">プロフィール</a>
+            </li>
+            <li>
+              <a href="#skill">スキル</a>
+            </li>
+            <li>
+              <a href="#works">制作物</a>
+            </li>
+            <li>
+              <a href="#contact">連絡先</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
