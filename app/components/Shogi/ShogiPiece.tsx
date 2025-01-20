2009f9c1b02f17bef5dcbf3783332f689e2344e7
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-20 13:39:13 +0900

    update: 駒名を縦書きで表示するよう変更

--- app/components/Shogi/ShogiPiece.tsx
+++ app/components/Shogi/ShogiPiece.tsx
@@ -4,7 +4,7 @@ interface ShogiPieceProps {
 
 export const ShogiPiece = ({ name }: ShogiPieceProps) => {
   return (
-    <div className="w-[50px] h-[60px] bg-cover bg-[url('/shogi/mokume.png')] clip-path-custom-shape flex items-center justify-center font-serif text-[24px] font-bold text-black">
+    <div className="w-[50px] h-[60px] bg-cover bg-[url('/shogi/mokume.png')] clip-path-custom-shape flex items-center justify-center font-serif text-[24px] font-bold text-black [writing-mode:vertical-rl]">
       {name}
     </div>
   );
