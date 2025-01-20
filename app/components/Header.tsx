e106431ac4a18cf04e917d25565fdad524dd4f91
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-20 14:15:34 +0900

    update: 駒名の長さに応じて、文字サイズを変更

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,15 +17,15 @@ export const Header: React.FC = () => {
           <ul className="flex justify-center items-center gap-4">
             <li>
               <a href="#profile">プロフィール</a>
-              <ShogiPiece name="歩" />
+              <ShogiPiece name="我" />
             </li>
             <li>
               <a href="#skill">スキル</a>
-              <ShogiPiece name="歩" />
+              <ShogiPiece name="技術" />
             </li>
             <li>
               <a href="#works">制作物</a>
-              <ShogiPiece name="歩" />
+              <ShogiPiece name="制作" />
             </li>
             <li>
               <a href="#contact">連絡先</a>
--- app/components/Shogi/ShogiPiece.tsx
+++ app/components/Shogi/ShogiPiece.tsx
@@ -3,9 +3,18 @@ interface ShogiPieceProps {
 }
 
 export const ShogiPiece = ({ name }: ShogiPieceProps) => {
+  // 文字数に応じて文字サイズを変更
+  const textSizeClass =
+    name.length === 1
+      ? "text-[24px]"
+      : name.length === 2
+      ? "text-[22px]"
+      : name.length === 3
+      ? "text-[18px]"
+      : "text-[12px]";
   return (
-    <div className="w-[50px] h-[60px] bg-cover bg-[url('/shogi/mokume.png')] clip-path-custom-shape flex items-center justify-center font-serif text-[24px] font-bold text-black [writing-mode:vertical-rl] pt-1">
-      {name}
+    <div className="w-[50px] h-[60px] bg-cover bg-[url('/shogi/mokume.png')] clip-path-custom-shape flex items-center justify-center font-serif text-[24px] font-bold text-black [writing-mode:vertical-rl] pt-1.5">
+      <span className={textSizeClass}>{name}</span>
     </div>
   );
 };
