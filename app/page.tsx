4e4439c54e035309991d3b687f0666078caeaf77
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-23 00:24:07 +0900

    update: ページ内スクロールにかかる時間にばらつきが出ないように、セクションの最小の高さを確保

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,56 @@
-import Image from "next/legacy/image";
+// import Image from "next/legacy/image";
 
 export default function PostPage() {
   return (
     <>
       <div className="pt-20">
-        <div className="relative w-full h-64">
+        {/* // 画像使うか未定 */}
+        {/* <div className="relative w-full h-64">
           <Image
             src="/home_imgs/travel_lufy.jpeg"
             alt="header"
             layout="fill"
             objectFit="cover"
           />
-        </div>
-        <section id="profile">
+        </div> */}
+
+        <section id="top" className="min-h-screen">
+          <div>
+            <h2 className="p-10">学生エンジニア有留のポートフォリオサイト</h2>
+            <div className="p-4">
+              <p>
+                私の学生エンジニアとしての人生は、将棋の対局のようなものだと思います。
+              </p>
+              <p>
+                序盤は学びの時期、大学のプログラミング演習や情報系の講義などで基本を一手ずつ積み重ねることで、自分の盤面を整えました。
+                中盤では、Web系のアルバイトやETロボコンなどの新しい挑戦が増え、試行錯誤しながら自分の盤面を広げました。
+                そして今、大学院生として中終盤に差し掛かり、次の一手一手が人生の勝負手となる重要な局面にいます。
+              </p>
+              <p>
+                これまでに積み上げてきた経験という駒たちを活かし、盤面全体を見渡しながら、自分にとっての最善手を見つけ出し、
+                そして、その一手が自分にとっての勝負手となるように、これからも一手ずつ努力を積み重ねていきます。
+              </p>
+            </div>
+          </div>
+        </section>
+        <section id="profile" className="min-h-screen">
           <h1>プロフィール</h1>
           <p>
             宮崎大学大学院工学研究科工学専攻先端情報コース修士1年の有留直希です。
           </p>
         </section>
-        <section id="profile">
+        <section id="skill" className="min-h-screen">
           <h1>スキル</h1>
           <p>
             C, C++, Python, JavaScript, TypeScript, React, Next.js, Tailwind
             CSS, Flask, MySQL, PostgreSQL, Git, Docker
           </p>
         </section>
-        <section id="profile">
+        <section id="works" className="min-h-screen">
           <h1>制作物</h1>
           <p>OnFes, Tacktail</p>
         </section>
-        <section id="profile">
+        <section id="contact" className="min-h-screen">
           <h1>連絡先</h1>
           <ul className="list-disc ml-8">
             <li>
