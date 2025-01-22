1de552e47d4363d17928c06afcaf5710e66116be
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-22 22:56:46 +0900

    update: ページ内スクロール遷移機能を実装

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { ShogiPiece } from "@components/Shogi/ShogiPiece";
+import { Link as ScrollLink } from "react-scroll";
 
 export const Header: React.FC = () => {
   return (
@@ -6,24 +9,56 @@ export const Header: React.FC = () => {
       <nav>
         <ul className="flex justify-end gap-4 fixed z-2 top-0 pt-2.5 pr-4 bg-gradient-to-b from-yellow-500 to-slate-200 w-full h-20">
           <li>
-            <a href="#profile" title="Profile">
-              <ShogiPiece name="我" />
-            </a>
+            <div className="cursor-pointer">
+              <ScrollLink
+                to="profile"
+                smooth={true}
+                duration={1000}
+                offset={-80}
+                spy={true}
+              >
+                <ShogiPiece name="我" />
+              </ScrollLink>
+            </div>
           </li>
           <li>
-            <a href="#skill" title="Skill">
-              <ShogiPiece name="技" />
-            </a>
+            <div className="cursor-pointer">
+              <ScrollLink
+                to="skill"
+                smooth={true}
+                duration={1000}
+                offset={-80}
+                spy={true}
+              >
+                <ShogiPiece name="技" />
+              </ScrollLink>
+            </div>
           </li>
           <li>
-            <a href="#works" title="Works">
-              <ShogiPiece name="開発" />
-            </a>
+            <div className="cursor-pointer">
+              <ScrollLink
+                to="works"
+                smooth={true}
+                duration={1000}
+                offset={-80}
+                spy={true}
+              >
+                <ShogiPiece name="開発" />
+              </ScrollLink>
+            </div>
           </li>
           <li>
-            <a href="#contact" title="Contact">
-              <ShogiPiece name="＠" />
-            </a>
+            <div className="cursor-pointer">
+              <ScrollLink
+                to="contact"
+                smooth={true}
+                duration={1000}
+                offset={-80}
+                spy={true}
+              >
+                <ShogiPiece name="＠" />
+              </ScrollLink>
+            </div>
           </li>
         </ul>
       </nav>
