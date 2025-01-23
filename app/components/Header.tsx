a63ad967dec29248076fb772bab4f6b08b067c31
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-23 23:25:11 +0900

    refactor: 新規にナビゲーションメニューを追加しやすいよう、cssを修正

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = () => {
     <header className="bg-primary w-full h-20">
       <nav>
         <ul className="flex justify-end fixed z-2 top-0 bg-primary opacity-75 w-full h-20 border-2 border-black">
-          <li className="border-l-2 border-r-2 border-black pt-1.5 px-2">
+          <li className="border-l-2 border-black pt-1.5 px-2">
             <div className="cursor-pointer">
               <ScrollLink
                 to="profile"
@@ -21,7 +21,7 @@ export const Header: React.FC = () => {
               </ScrollLink>
             </div>
           </li>
-          <li className="border-r-2 border-black pt-1.5 px-2">
+          <li className="border-l-2 border-black pt-1.5 px-2">
             <div className="cursor-pointer">
               <ScrollLink
                 to="skill"
@@ -34,7 +34,7 @@ export const Header: React.FC = () => {
               </ScrollLink>
             </div>
           </li>
-          <li className="border-r-2 border-black pt-1.5 px-2">
+          <li className="border-l-2 border-black pt-1.5 px-2">
             <div className="cursor-pointer">
               <ScrollLink
                 to="works"
@@ -47,7 +47,7 @@ export const Header: React.FC = () => {
               </ScrollLink>
             </div>
           </li>
-          <li className="pt-1.5 px-2">
+          <li className="border-l-2 border-black pt-1.5 px-2">
             <div className="cursor-pointer">
               <ScrollLink
                 to="contact"
