ef0f65bc8d7cd398b7cd2a42a981c9c5c8e60838
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-28 03:50:54 +0900

    add: コード整形のパッケージの導入、設定

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+    semi: true, // セミコロン無し
+    singleQuote: true, // シングルクォート使う
+    printWidth: 90, // 折り返し
+    tabWidth: 4, // スペースの単位
+    trailingComma: 'all', // ケツカンマつける
+    jsxSingleQuote: true, //　JSXでシングルクォートを使用
+};
