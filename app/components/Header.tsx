81b6ad23e6a4588557c181fd0229cda18820821a
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-01-30 00:37:32 +0900

    update: ヘッダーの重なり順を開発物紹介カードよりも上になるよう調整

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header: React.FC = () => {
     return (
         <header className='bg-primary w-full h-20'>
             <nav>
-                <ul className='flex justify-end fixed z-2 top-0 bg-primary opacity-75 w-full h-20 border-2 border-black'>
+                <ul className='flex justify-end fixed z-10 top-0 bg-primary opacity-75 w-full h-20 border-2 border-black'>
                     <li className='border-l-2 border-black pt-1.5 px-2'>
                         <div className='cursor-pointer'>
                             <ScrollLink
