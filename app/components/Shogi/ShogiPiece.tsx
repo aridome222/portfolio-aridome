64a88fdc2b258588a32e8a6ff100d247c1a5e609
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-03-17 10:41:35 +0900

    add: 選択された駒のテキスト色を紅色に変更し、それ以外の駒のテキスト色を黒色に変更する処理を愚直に実装

--- app/components/Shogi/ShogiPiece.tsx
+++ app/components/Shogi/ShogiPiece.tsx
@@ -1,8 +1,9 @@
 type ShogiPieceProps = {
-    name: string;
+    name: string; // 駒名
+    textColor: string; // 選択されたときの駒の色
 };
 
-export const ShogiPiece = ({ name }: ShogiPieceProps) => {
+export const ShogiPiece = ({ name, textColor }: ShogiPieceProps) => {
     // 文字数に応じて文字サイズを変更
     const textSizeClass =
         name.length === 1
@@ -12,11 +13,12 @@ export const ShogiPiece = ({ name }: ShogiPieceProps) => {
               : name.length === 3
                 ? 'text-[18px]'
                 : 'text-[12px]';
+
     return (
         <div className='relative flex h-[60px] w-[50px] items-center justify-center'>
             <div className='absolute inset-0 bg-black clip-path-custom-shape-inner'>
                 <div className='flex size-full items-center justify-center bg-amber-400 pr-0.5 pt-1.5 font-serif font-bold text-black clip-path-custom-shape [writing-mode:vertical-rl]'>
-                    <span className={textSizeClass}>{name}</span>
+                    <span className={`${textColor} ${textSizeClass}`}>{name}</span>
                 </div>
             </div>
         </div>
--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,9 +1,39 @@
 'use client';
 
+import { useState } from 'react';
 import { ShogiPiece } from '@components/Shogi/ShogiPiece';
 import { Link as ScrollLink } from 'react-scroll';
 
 export const Header: React.FC = () => {
+    const [textColor1, setTextColor1] = useState('text-black');
+    const [textColor2, setTextColor2] = useState('text-black');
+    const [textColor3, setTextColor3] = useState('text-black');
+    const [textColor4, setTextColor4] = useState('text-black');
+    const changeTextColor1 = () => {
+        setTextColor1('text-beni');
+        setTextColor2('text-black');
+        setTextColor3('text-black');
+        setTextColor4('text-black');
+    };
+    const changeTextColor2 = () => {
+        setTextColor1('text-black');
+        setTextColor2('text-beni');
+        setTextColor3('text-black');
+        setTextColor4('text-black');
+    };
+    const changeTextColor3 = () => {
+        setTextColor1('text-black');
+        setTextColor2('text-black');
+        setTextColor3('text-beni');
+        setTextColor4('text-black');
+    };
+    const changeTextColor4 = () => {
+        setTextColor1('text-black');
+        setTextColor2('text-black');
+        setTextColor3('text-black');
+        setTextColor4('text-beni');
+    };
+
     return (
         <header className='h-20 w-full bg-primary'>
             <nav>
@@ -16,8 +46,9 @@ export const Header: React.FC = () => {
                                 duration={1000}
                                 offset={-80}
                                 spy={true}
+                                onClick={changeTextColor1}
                             >
-                                <ShogiPiece name='我' />
+                                <ShogiPiece name='我' textColor={textColor1} />
                             </ScrollLink>
                         </div>
                     </li>
@@ -29,8 +60,9 @@ export const Header: React.FC = () => {
                                 duration={1000}
                                 offset={-80}
                                 spy={true}
+                                onClick={changeTextColor2}
                             >
-                                <ShogiPiece name='技術' />
+                                <ShogiPiece name='技術' textColor={textColor2} />
                             </ScrollLink>
                         </div>
                     </li>
@@ -42,8 +74,9 @@ export const Header: React.FC = () => {
                                 duration={1000}
                                 offset={-80}
                                 spy={true}
+                                onClick={changeTextColor3}
                             >
-                                <ShogiPiece name='開発' />
+                                <ShogiPiece name='開発' textColor={textColor3} />
                             </ScrollLink>
                         </div>
                     </li>
@@ -55,8 +88,9 @@ export const Header: React.FC = () => {
                                 duration={1000}
                                 offset={-80}
                                 spy={true}
+                                onClick={changeTextColor4}
                             >
-                                <ShogiPiece name='＠' />
+                                <ShogiPiece name='＠' textColor={textColor4} />
                             </ScrollLink>
                         </div>
                     </li>
